28e4a031e6cd078291636339c2996f18a0d0cdad
Sara0921 <salahsara360@gmail.com>
2026-03-30 23:55:00 +0200

    Question03

--- Assignment 1/Program.cs
+++ Assignment 1/Program.cs
@@ -1,12 +1,28 @@
 ﻿namespace Assignment_1
 {
     #region Question02
-   //public class Container<T>
-   // {
-   //     private List<T> _items = new();
-   //     public void Add(T item) => _items.Add(item);
-   //     public T Get(int index) =>_items[index];
-   // }
+    //public class Container<T>
+    // {
+    //     private List<T> _items = new();
+    //     public void Add(T item) => _items.Add(item);
+    //     public T Get(int index) =>_items[index];
+    // }
+    #endregion
+    #region Question03
+    //A generic class uses type parameters that are replaced with actual types when you create an instance. The type parameter T acts as a placeholder.
+    //public class Pair<TKey , TValue>
+    //{
+    //    public TKey Key { get; }
+    //    public TValue Value { get; }
+    //    public Pair(TKey key, TValue value)
+    //    {
+    //        Key = key;
+    //        Value = value;
+    //    }
+    //    public override string ToString() => $"{Key} : {Value}";
+       
+ 
+    //}
     #endregion
     internal class Program
     {
@@ -19,6 +35,12 @@
             //Performance >> No boxing / unboxing for value types
             //Code Reuse >>  One implementation for all types
             //IntelliSense >>    Better IDE support and discovery
+            #endregion
+
+            //Usage
+            #region Question03
+            //var Pair = new Pair<string, int>("Score", 99);
+            //Console.WriteLine(Pair);
             #endregion
         }
     }
