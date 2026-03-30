a426e76b3a678cffd69feafd7b0c8b2be753e0ba
Sara0921 <salahsara360@gmail.com>
2026-03-31 00:49:54 +0200

    Question15

--- Assignment 1/Program.cs
+++ Assignment 1/Program.cs
@@ -193,6 +193,10 @@ namespace Assignment_1
     //Covariance allows you to use a more derived type than originally specified.
     //Marked with out keyword. T can only appear in output positions.
     #endregion
+    #region Question15
+    //Contravariance allows you to use a less derived type than originally specified.
+    //Marked with in keyword. T can only appear in input positions.
+    #endregion
     internal class Program
     {
         static void Main(string[] args)
