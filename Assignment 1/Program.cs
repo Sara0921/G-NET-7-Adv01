39eec4a27491150b3ab9f2a684fb43b4b2f5daaf
Sara0921 <salahsara360@gmail.com>
2026-03-30 23:25:48 +0200

    Question01

--- Assignment 1/Program.cs
+++ Assignment 1/Program.cs
@@ -4,6 +4,14 @@
     {
         static void Main(string[] args)
         {
+            #region Question01
+            //A generic class uses type parameters that are replaced with actual types when you create an instance. The type parameter T acts as a placeholder.
+            //Benefit >>	Description
+            //Type Safety >> Compile - time type checking
+            //Performance >> No boxing / unboxing for value types
+            //Code Reuse >>  One implementation for all types
+            //IntelliSense >>    Better IDE support and discovery
+            #endregion
         }
     }
 }
